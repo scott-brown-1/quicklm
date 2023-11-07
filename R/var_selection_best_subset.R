e0e9003798332f614a3a8fe2bd4f600c1d9bc268
Scott Brown <scottdbrown7@gmail.com>
2023-11-06 18:05:00 -0700

    Improve stepwise var selection

--- R/var_selection_best_subset.R
+++ R/var_selection_best_subset.R
@@ -23,14 +23,3 @@ var_selection_best_subset <- function(model, criteria='AIC', return_model=F){
   print(summary(best_subsets_mod$BestModel))
   if(return_model) return(best_subsets_mod)
 }
-
-data(trees)
-tree_model <- lm(Volume ~ Height + Girth, data=trees)
-var_selection_best_subset(tree_model, criteria='AIC', return_model=F)
-
-
-
-height      -0.44669    0.10406  -4.293 2.55e-05 ***
-  neck        -0.48479    0.17997  -2.694  0.00755 **
-  chest       -0.14382    0.08160  -1.762  0.07924 .
-abdom        0.82586    0.06056  13.638  < 2e-16 ***
